8bfa6ade95f6bf1326717685de112c3487725cf1
Eduarda <eduardaliz11@gmail.com>
2026-01-17 22:10:46 -0300

    Add files via upload

--- Caneta.java
+++ Caneta.java
@@ -1,30 +1,66 @@
-public class Caneta {
-    public String cor;
-    public String modelo;
-    public double ponta;
-    protected int carga; 
-    private boolean tampada; //PRIVADO - SÓ QUEM MEXE É A PROPRIA CLASSE!!!
+package aula04;
 
-    public void status() {
-        System.out.println("Modelo: " + this.modelo);
-        System.out.println("Cor: " + this.cor);
-        System.out.println("está tampada? " + this.tampada);
-        System.out.println("Carga: " + this.carga);
-        System.out.println("Ponta: " + this.ponta);
+public class Caneta {
+    private String modelo;
+    private double ponta;
+    private boolean tampada;
+    private String cor;
+ 
+    public Caneta(String m, String c, double p) { //METODO CONSTRUTOR == MESMO NOME DA CLASSE!!!
+        this.modelo = m;  
+        this.ponta = p;  
+        this.tampar();
+        this.cor = c;
     }
-    public void rabiscar() {
-        if (this.tampada == true) {
-            System.out.println("Não posso rabiscar estou tampada");
-        } else {
-            System.out.println("Estou rabiscando");
-        }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String m) {
+        this.modelo = m;
+    }
+
+
+    public double getPonta() {
+        return this.ponta;
+    }
+
+    public void setPonta(double p) {
+        this.ponta = p; 
     }
 
     public void tampar() {
-        this.tampada = true; //No "Private" só quem pode mexer é a própria classe, por isso é possível tampar e destampar
-    } 
+        this.tampada = true;
+    }
 
     public void destampar() {
         this.tampada = false;
+    }
+
+    public String getCor() {
+        return this.cor;
+    }
+
+    public void setCor(String c) {
+        cor = c;
+    }
+
+    public boolean getTampada() {
+        return this.tampada;
+    }
+
+    public void setTampada(boolean t){
+        tampada = t;
+    
+    }
+
+        
+    public void status() {
+        System.out.println("SOBRE A CANETA");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+        System.out.println("Cor: " + this.getCor());
+        System.out.println("Está tampada? " + this.getTampada());
     }
 }
